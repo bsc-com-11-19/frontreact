ce0c69d3326645574f2389449f74fb2c2f3decdb
bsc-com-11-19 <bsc-com-11-19@unima.ac.mw>
2022-02-23 01:16:42 +0200

    webpage enhanced abit

---
+++ src/component.js
@@ -0,0 +1,12 @@
+import ReactDOM from 'react-dom';
+function MyForm() {
+    return (
+      <form>
+        <label>Enter your name:
+          <input type="text" />
+        </label>
+      </form>
+    )
+  }
+  ReactDOM.render(<MyForm />, document.getElementById('root'));
+  
--- src/index.js
+++ src/index.js
@@ -9,10 +9,19 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('Nav')
+  
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+  
+);
+
+
+
 reportWebVitals();
